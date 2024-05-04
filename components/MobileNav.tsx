ae948d4f70d89f40027328cb7e5c81fef50d19e6
kentashi <kenta.nam.97@gmail.com>
2024-05-04 23:56:49 +0700

    feat: clerk, stream

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -49,10 +49,9 @@ const MobileNav = () => {
                                     const isActice = pathName === link.route
 
                                     return (
-                                        <SheetClose asChild>
+                                        <SheetClose key={link.label} asChild>
                                             <Link
                                                 href={link.route}
-                                                key={link.label}
                                                 className={cn(
                                                     "flex gap-4 items-center p-4 rounded-lg w-full max-w-60 ",
                                                     { "bg-blue-1": isActice }
---
+++ providers/StreamClientProvider.tsx
@@ -0,0 +1,32 @@
+import { tokenProvider } from "@/actions/stream.actions"
+import { useUser } from "@clerk/nextjs"
+import { StreamVideo, StreamVideoClient } from "@stream-io/video-react-sdk"
+import { ReactNode, useState, useEffect } from "react"
+
+const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY
+
+const StreamVideoProvider = ({ children }: { children: ReactNode }) => {
+    const [videoClient, setVideoClient] = useState<StreamVideoClient>()
+    const { user, isLoaded } = useUser()
+
+    useEffect(() => {
+        if (!user || !isLoaded) return
+        if (!apiKey) throw new Error("Stream Api Key missing")
+
+        const client = new StreamVideoClient({
+            apiKey,
+            user: {
+                id: user?.id,
+                name: user?.username || user?.id,
+                image: user?.imageUrl,
+            },
+            tokenProvider,
+        })
+
+        setVideoClient(client)
+    }, [user, isLoaded])
+
+    return <StreamVideo client={videoClient}></StreamVideo>
+}
+
+export default StreamVideoProvider
